db29533026b082fb38c9c045775ba80f151bbc21
Eric Romero <eric.romero.mtz@gmail.com>
2023-09-12 13:36:13 -0600

    Se agrega iconito para acceso a cartelera

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,14 +3,24 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
+                <!-- Logos -->
                 <div class="shrink-0 flex items-center">
                     {{-- <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a> --}}
-                    <a href="{{ route('dashboard') }}">
-                        <img src="{{ asset('images/50anios.png') }}" alt="G-RES" class="block h-10 w-auto sm:rounded-lg" />
-                    </a>
+                    <div>
+                        <a href="{{ route('eventos.cartelera') }}">
+                            <img src="{{ asset('images/cartelera.png') }}" alt="G-RES" class="block h-10 w-auto sm:rounded-lg" />
+                        </a>
+                    </div>
+                    
+                    <div class="ml-4">
+                        <a href="{{ route('dashboard') }}">
+                            <img src="{{ asset('images/50anios.png') }}" alt="G-RES" class="block h-10 w-auto sm:rounded-lg" />
+                        </a>
+                    </div>
+                    
+                    
                     
                 </div>
 
